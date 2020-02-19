bc4a79499803a90b288a85d09e31490257669b53
babiswas <36biswas0814@gmail.com>
2020-02-19 22:07:21 +0530

    message

--- test1.py
+++ test1.py
@@ -1,15 +1,9 @@
 import re
+string="Hello Brother What are you doing\n The body is er lying on the table\n How are you other dancer"
+print(re.search('ot',string))
+print(re.findall('ot',string))
+print(re.findall('\w*er[\s]',string))
 
-string="""The regex Bat(wo)+man will not match the string ‘The Adventures of Batman’ because at least one wo is required by the plus sign.\n
-If you need to match an actual plus sign character, prefix the plus sign with a backslash to escape it: \+.\n
-This article is contributed by Shubham Machal. If you like GeeksforGeeks and would like to contribute, you can also write an article using contribute.geeksforgeeks.org or mail your article to contribute@geeksforgeeks.org. See your article appearing on the GeeksforGeeks main page and help other Geeks.\n
-Please write comments if you find anything incorrect, or you want to share more information about the topic discussed above"""
-
-
-#^ is begining of the string
-#$ is the end of the string
-
-print(re.search('^Please Write\s',string)
 
 
 
